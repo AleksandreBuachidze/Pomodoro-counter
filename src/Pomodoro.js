75608db143595dabfd4121f3a5eec8fd43c79816
dortaxx <akhvledianiluka37@gmail.com>
2022-02-12 20:54:48 +0400

    ff

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,134 +1,165 @@
-import React from 'react'
-import {useState,useEffect} from 'react'
-import beep from './sound/beep.mp3';
-import {Howl, Howler} from 'howler';
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import beep from "./sound/beep.mp3";
+import { Howl, Howler } from "howler";
 
 const Pomodoro = () => {
-  const[minutes,setMinutes] = useState(25);
-  const[seconds,setSeconds] = useState(0);
-  const[displayMessage,setDisplayMessage] = useState(false)
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [displayMessage, setDisplayMessage] = useState(false);
   const [start, setStart] = useState(false);
-  const [breakTime,setBreakTime] = useState(5)
-  const [disabled,  setDisabled] = useState(false)
+  const [breakTime, setBreakTime] = useState(5);
+  const [disabled, setDisabled] = useState(false);
 
-
+  const reset = (e) => {
  
 
-  const  reset =(e) =>{
-    // window.location.reload(false);
-  
-setStart(false)
+    setStart(false);
 
-setTimeout(() => {
-  setMinutes(25)
-  setSeconds(0)
-  setBreakTime(5)
-  setDisabled(true)
-  setTimeout(()=>{setDisabled(false)},500)
-}, 1000);
-   
-   }
+    setTimeout(() => {
+      setMinutes(25);
+      setSeconds(0);
+      setBreakTime(5);
+      setDisabled(true);
+      setTimeout(() => {
+        setDisabled(false);
+      }, 500);
+    }, 1000);
+  };
 
   useEffect(() => {
     let interval = setInterval(() => {
       clearInterval(interval);
-      if(start){
-        setDisabled(true)
-        
-      if(seconds === 0){
-if(minutes !==0){
-setSeconds(59)
-setMinutes(minutes-1)
+      if (start) {
+        setDisabled(true);
 
-} else {
-  setStart(false)
-  if(start){
-   let  min = displayMessage ? 24 : breakTime;
-   let sec = 0
-  
-   setSeconds(sec)
-   setMinutes(min)
-   setDisplayMessage(!displayMessage)
-   if(displayMessage){
-     setDisabled(false)
-   }
-  }
-   
-}
-      }else {
-        setSeconds(seconds-1)
-      } 
-    } 
-    
-    }
-    ,1000)
-  
+        if (seconds === 0) {
+          if (minutes !== 0) {
+            setSeconds(59);
+            setMinutes(minutes - 1);
+          } else {
+            setStart(false);
+            if (start) {
+              let min = displayMessage ? 24 : breakTime;
+              let sec = 0;
 
-   
-  }, [seconds,start,])
+              setSeconds(sec);
+              setMinutes(min);
+              setDisplayMessage(!displayMessage);
+              if (displayMessage) {
+                setDisabled(false);
+              }
+            }
+          }
+        } else {
+          setSeconds(seconds - 1);
+        }
+      }
+    }, 1000);
+  }, [seconds, start]);
 
   const timer = minutes < 10 ? `0${minutes}` : minutes;
   const timerSec = seconds < 10 ? `0${seconds}` : seconds;
 
-  const handleInc = (e) =>{
-    if(minutes < 60 ){
-      setMinutes(minutes + 1)
+  const handleInc = (e) => {
+    if (minutes < 60) {
+      setMinutes(minutes + 1);
     } else {
-      setMinutes(0)
+      setMinutes(0);
     }
-  }
+  };
   const handleDec = (e) => {
-    if(minutes > 0){
-      setMinutes(minutes - 1)
+    if (minutes > 0) {
+      setMinutes(minutes - 1);
     } else {
-      setMinutes(0)
+      setMinutes(0);
     }
-  }
+  };
 
   const breakInc = (e) => {
-   if(breakTime < 60){
-     setBreakTime(breakTime + 1)
-   } else {
-     setBreakTime(0)
-   }
-  }
+    if (breakTime < 60) {
+      setBreakTime(breakTime + 1);
+    } else {
+      setBreakTime(0);
+    }
+  };
 
   const breakDec = (e) => {
-    if(breakTime > 0){
-      setBreakTime(breakTime -1)
+    if (breakTime > 0) {
+      setBreakTime(breakTime - 1);
     } else {
-      setBreakTime(0)
+      setBreakTime(0);
     }
-   }
-  
+  };
+
   return (
-    <div className='pomodoro'>
-        {!displayMessage && <div> <p>Break:{breakTime}</p>
-      <button disabled={disabled} className='button-27' onClick={breakInc}>+</button>
-      <button  disabled={disabled} className='button-27' onClick={breakDec}>-</button>
-      </div>}
-      <div className='msg'>
-   
-      <div className='timer'>
-{timer}:{timerSec}
-
-{displayMessage && <div>
-        Break time !
-           <p>Press Start</p>
-
-            <button  className='button-27'   onClick={() => {setStart(true)}}>Start</button>
-
-      </div>}
-{!displayMessage && <button disabled={disabled} className='button-27' onClick={handleInc}>+</button>}
-{!displayMessage && <button disabled={disabled} className='button-27'   onClick={handleDec}>-</button>}
-{!displayMessage && <button  className='button-27'  disabled={disabled} onClick={() => {setStart(true)}}>Start</button>}
- {!displayMessage && <button   className='button-27' onClick={reset}>Reset</button> }
+    <div className="pomodoro">
+      {!displayMessage && (
+        <div>
+          {" "}
+          <p>Break:{breakTime}</p>
+          <button disabled={disabled} className="button-27" onClick={breakInc}>
+            +
+          </button>
+          <button disabled={disabled} className="button-27" onClick={breakDec}>
+            -
+          </button>
+        </div>
+      )}
+      <div className="msg">
+        <div className="timer">
+          {timer}:{timerSec}
+          {displayMessage && (
+            <div>
+              Break time !<p>Press Start</p>
+              <button
+                className="button-27"
+                onClick={() => {
+                  setStart(true);
+                }}
+              >
+                Start
+              </button>
+            </div>
+          )}
+          {!displayMessage && (
+            <button
+              disabled={disabled}
+              className="button-27"
+              onClick={handleInc}
+            >
+              +
+            </button>
+          )}
+          {!displayMessage && (
+            <button
+              disabled={disabled}
+              className="button-27"
+              onClick={handleDec}
+            >
+              -
+            </button>
+          )}
+          {!displayMessage && (
+            <button
+              className="button-27"
+              disabled={disabled}
+              onClick={() => {
+                setStart(true);
+              }}
+            >
+              Start
+            </button>
+          )}
+          {!displayMessage && (
+            <button className="button-27" onClick={reset}>
+              Reset
+            </button>
+          )}
+        </div>
       </div>
-      </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Pomodoro
+export default Pomodoro;
